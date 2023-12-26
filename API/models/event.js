58e9724aaba6d4e3585bac55ed447c9221413a35
pauldereep <dereeper.paul@icloud.com>
2023-12-26 16:50:26 +0100

    ajout de event.js dans les models

---
+++ API/models/event.js
@@ -0,0 +1,31 @@
+const {Model, DataType} = require("sequelize");
+const connection = require("./db");
+const bcrypt = require("bcrypt");
+
+class Event extends Model {}
+
+Event.init
+({
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+
+    typeOfEvent: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+
+    typeOfEvent: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          isIn: [['sport', 'fashion', 'music', 'business','education', 'politcs']]
+        },
+      },
+
+})
+
+
+
+

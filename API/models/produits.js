be931e7559c59a88e8858317a0ca4bfc5731d11d
nicolasbelas <151186470+nicolasbelas@users.noreply.github.com>
2023-12-26 17:13:00 +0100

    Create produits.js

---
+++ API/models/produits.js
@@ -0,0 +1,40 @@
+const { Model, DataTypes } = require("sequelize");
+const connection = require("./db");
+const bcrypt = require("bcryptjs");
+
+class Product extends Model {}
+
+Product.init(
+    {
+        id_produit: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false,
+        },
+        nom: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        prix: {
+            type: DataTypes,DECIMAL(10, 2),
+            allowNull: false,
+            validate: {
+                min: 0,
+            },
+        },
+        quantite: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validate: {
+                min: 0,
+            },
+        },
+    },
+    {
+        sequelize: connection,
+        modelName: 'Product',
+    }
+);
+
+module.exports= Product;

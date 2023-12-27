ae89ee168d9d47eeae239857f86b5bd923c80ef1
ldllc <115065483+ldllc@users.noreply.github.com>
2023-12-27 22:21:06 +0000

    modification des noms lieu

--- API/routes/lieuRoute.js
+++ API/routes/lieuRoute.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Lieu = require('../models/Lieu');
+const Lieu = require('../models/lieu');
 
 // Get all lieux
 router.get('/', async (req, res) => {

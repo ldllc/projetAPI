1a70eb8eb64a6acc3c5693aa0aaf67a40f4dbea5
ldllc <115065483+ldllc@users.noreply.github.com>
2023-12-27 22:17:50 +0000

    ajout du fichier routeLieu

---
+++ API/routes/routeLieu.js
@@ -0,0 +1,92 @@
+const express = require('express');
+const router = express.Router();
+const Lieu = require('../models/Lieu');
+
+// Get all lieux
+router.get('/', async (req, res) => {
+  try {
+    const lieux = await Lieu.findAll();
+    res.json(lieux);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+// Get a specific lieu by id
+router.get('/:id', async (req, res) => {
+  const lieuId = req.params.id;
+
+  try {
+    const lieu = await Lieu.findByPk(lieuId);
+
+    if (!lieu) {
+      return res.status(404).send('Lieu not found');
+    }
+
+    res.json(lieu);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+// Create a new lieu
+router.post('/', async (req, res) => {
+  const { nom, adresse } = req.body;
+
+  try {
+    const newLieu = await Lieu.create({ nom, adresse });
+    res.json(newLieu);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+// Update a lieu by id
+router.put('/:id', async (req, res) => {
+  const lieuId = req.params.id;
+  const { nom, adresse } = req.body;
+
+  try {
+    const lieu = await Lieu.findByPk(lieuId);
+
+    if (!lieu) {
+      return res.status(404).send('Lieu not found');
+    }
+
+    // Update the lieu attributes
+    lieu.nom = nom;
+    lieu.adresse = adresse;
+
+    await lieu.save();
+
+    res.json(lieu);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+// Delete a lieu by id
+router.delete('/:id', async (req, res) => {
+  const lieuId = req.params.id;
+
+  try {
+    const lieu = await Lieu.findByPk(lieuId);
+
+    if (!lieu) {
+      return res.status(404).send('Lieu not found');
+    }
+
+    await lieu.destroy();
+
+    res.send('Lieu deleted successfully');
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+module.exports = router;
